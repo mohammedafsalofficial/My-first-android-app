ca6b332dc424f6997eb41d6c2e2d9759babe9b7f
mohammedafsalofficial <mohammedafsaltut@gmail.com>
2023-04-06 10:16:10 +0530

    first commit

---
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.greetingsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Hello " + name, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
